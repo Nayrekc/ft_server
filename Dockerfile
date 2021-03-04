185727a7ea3cd45d891696cc60c9801ea57f5045
Keryan Taouki <ketaouki@z3r2p3.42lyon.fr>
2021-03-04 14:25:49 +0100

    update, nginx + php okay. phpAdmin + Mysql no okay

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,19 @@
-FROM debian:buster-slim
+FROM debian:buster
 
-RUN apt-get -y update && apt-get -y install mariadb-server \
-		wget \
-		php \
-		php-cli \
-		php-cgi \
-		php-mbstring \
-		php-fpm \
-		php-mysql \
-		nginx \
-		libnss3-tools \
-		openssl
-
+RUN apt-get update
+RUN apt-get install -y nginx
+RUN apt-get install -y mariadb-server mariadb-client
+RUN apt-get install -y php7.3 php7.3-fpm php7.3-mysql php-common php7.3-cli php7.3-common php7.3-json php7.3-opcache php7.3-readline
+RUN apt-get install -y php-mbstring php-zip php-gd
+RUN apt-get install -y php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip
+RUN apt-get install openssl
 
 COPY srcs/start.sh ./
 COPY srcs/default ./
+COPY srcs/index.html ./
+COPY srcs/test.php ./
+COPY srcs/config.inc.php ./
+
 
 CMD bash /start.sh
 
--- srcs/start.sh
+++ srcs/start.sh
@@ -1,18 +1,37 @@
 #!/bin/sh
 
 #phpMyAdmin
+wget https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-languages.tar.gz
+tar xvf phpMyAdmin-4.9.0.1-all-languages.tar.gz
+mv phpMyAdmin-4.9.0.1-all-languages var/www/localhost/phpmyadmin
+service php7.3-fpm start
+
 
 #MYSQL
 
+service mysql start
+echo "CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;" | mysql -u root
+echo "GRANT ALL ON wordpress.* TO 'wordpress_user'@'localhost' IDENTIFIED BY 'password';" | mysql -u root
+echo "FLUSH PRIVILEGES;" | mysql -u root
+
 #NGINX command
 mkdir var/www/localhost
+mv ./default etc/nginx/sites-available
+mv ./index.html var/www/localhost
+mv ./test.php var/www/localhost
 
 #SSL
-mkdir /etc/nginx/ssl
-openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/localhost.pem -keyout /etc/nginx/ssl/localhost.key -subj "/C=FR/ST=LYON/L=LYON/O=42 CN=localhost"
 
 #WORDPRESS
+wget https://wordpress.org/latest.tar.gz
+tar xvf latest.tar.gz
+mkdir var/www/localhost/wordpress
+cp -a wordpress/. /var/www/localhost/wordpress
+mv ./wp-config.php /var/www/localhost/wordpress
+
 
 #RUN
 service nginx start
+service php7.3-fpm restart
+service mysql restart
 sleep infinity
